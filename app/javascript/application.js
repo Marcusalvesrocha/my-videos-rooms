4e8df9a9ecbc717f74accb3f086f5f80d592e683
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-22 11:15:41 -0300

    project with react

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World to new video room with react delete controllers js!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
