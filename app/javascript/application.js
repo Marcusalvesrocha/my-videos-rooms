68c0ec5bdd066455559c52fc6652b094757a62bf
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-23 16:35:55 -0300

    Adding api of youtube to get videos

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from "./components/Menu/Menu";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Rooms from "./components/Rooms/Rooms";
 import Room from "./components/Room/Room";
--- app/javascript/components/Room/Room.js
+++ app/javascript/components/Room/Room.js
@@ -4,19 +4,52 @@ import { Link, useParams } from "react-router-dom";
 
 const Room = () => {
   const [room, setRoom] = useState({})
+  const [videos, setVideos] = useState([])
 
   const { id } = useParams();
 
   useEffect(()=>{
     axios.get(`/api/v1/rooms/${id}`)
-      .then(resp => setRoom(resp.data))
+      .then(resp => {
+        setRoom(resp.data)
+        getVideos()
+      })
       .catch(resp => console.log(resp))
+
   }, [])
+
+  const getVideos = () => {
+    const key = 'AIzaSyB_-mKJ7FMHVP962LNyHQ9jJeuanx5uGNo';
+    const part = 'snippet';
+    const type = 'video'
+    const q = room.title;
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${key}&q=${q}&part=${part}&type=${type}`
+
+    axios.get(url)
+    .then(resp => setVideos(resp.data.items))
+    .catch(resp => console.log(resp))
+  }
+
+  const handleVideos = videos.map(video => {
+
+    console.log('---------------------youtubeapi')
+    console.log(video.id.videoId)
+    return (
+      <li>
+        <iframe src={`https://www.youtube.com/embed/${video.id.videoId}`} title="The Best of Metallica (part 1)🎸Лучшие песни группы Metallica -1 часть🎸The Greatest Hits of Metallica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </li>
+    )
+  })
+
 
   return(
 
     <div>
-      <h1>sem console na Salas: { room.title }</h1>
+      <h1>{ room.title }</h1>
+      <ul>{handleVideos}</ul>
+      <iframe width="1280" height="661" src="https://www.youtube.com/embed/SqZNMvIEHhs?list=RDSqZNMvIEHhs" title="System Of A Down - Spiders (Official HD Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
       <Link to="/">Home</Link>
     </div>
   )
